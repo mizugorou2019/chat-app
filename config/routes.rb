fc6545f28c8422607bc0dd07ec66885233524e10
mizugorou2019 <mizugorou2019@users.noreply.github.com>
2020-08-18 12:42:44 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
-    resources :messages, only: [:index, :create]
+  resources :rooms, only: [:new, :create, :destroy] do
+    resources :messages, only: [:index, :create,]
     # roomsにmessagesが「ネスト」されている!
   end
 end
